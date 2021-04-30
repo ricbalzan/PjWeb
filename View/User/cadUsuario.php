1cdb02df1318126a0e7c8712730d661ce6c77f2d
ricbalzan <balzanricardo@gmail.com>
2021-04-29 21:51:22 -0300

    CSS
    inserção de CSS no cadastro

--- View/User/cadUsuario.php
+++ View/User/cadUsuario.php
@@ -3,12 +3,16 @@
     <head>
         <meta charset="utf-8">
         <title>Cadastro de Usuários</title>
+        <link rel="stylesheet" type="text/css" href="../templates/main.css" />
     </head>
 
     <body>
+<div>
+    <form action="../../Controller/UserController.php" method="post" action="" name="form_user">
 
-    <form action="../../Controller/UserController.php" method="post" name="form_user">
+    <h1>Cadastro de Usuários</h1>
 
+        
         <input type="text" name="txtCpf" id="txtCpf" placeholder="Cpf" /><br><br>
         <input type="text" name="txtNome" id="txtNome" placeholder="Nome" /><br><br>
         <input type="text" name="txtEmail" id="txtEmail" placeholder="Email" /><br><br>
@@ -20,7 +24,7 @@
         <input type="submit" value="Cadastrar"/>
         <input type="reset" value="Limpar"/>
     </form>
-     
+    </div>
     </body>
 
 </html>
