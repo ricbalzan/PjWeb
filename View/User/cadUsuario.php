8014e89c3518230a44f41bee0bfa53b9b5725451
Ricardo Balzan <49926270+ricbalzan@users.noreply.github.com>
2021-05-13 14:21:27 -0300

    bootstrap

--- View/User/cadUsuario.php
+++ View/User/cadUsuario.php
@@ -2,33 +2,47 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Cadastro de Usuários</title>
-        <link rel="stylesheet" type="text/css" href="../templates/main.css" />
+            <blockquote class="blockquote text-center">
+                <p class="mb-0"><h1>Cadastro de Usuários</h1></p>
+            </blockquote>
+        <link rel="stylesheet" type="text/css" href="../templates/bootstrap/css/bootstrap.css" />
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+
     </head>
+
+    
 
     <body>
 <div class="conteudo">
+    <div class="form-group">
     <form action="../../Controller/UserController.php" method="post" action="" name="form_user">
 
-    <h1>Cadastro de Usuários</h1>
+    
 
-        <div id="cpf">        
-        <input class="cpf" type="text" name="txtCpf" id="txtCpf" placeholder="Cpf" /><br><br>
+        <div id="cpf"> 
+        <input class="form-control" type="text" name="txtCpf" id="txtCpf" placeholder="Cpf" /><br><br>
         </div>
         <div id="nome"> 
-        <input class="nome" type="text" name="txtNome" id="txtNome" placeholder="Nome" /><br><br>
+        <input class="form-control" type="text" name="txtNome" id="txtNome" placeholder="Nome" /><br><br>
         </div>
         <div id="txtEmail"> 
-        <input type="text" name="txtEmail" id="txtEmail" placeholder="Email" /><br><br>
+        <input type="text" class="form-control" name="txtEmail" id="txtEmail" placeholder="Email" /><br><br>
         </div>
-        <input type="password" name="txtSenha" id="txtSenha" placeholder="Senha" /><br><br>
-        <input type="date" name="txtNascimento" id="txtNascimento" data-mask="00/00/0000" maxlength="8" placeholder="Nacimento DD/MM/AAAA" /><br><br>
-        <input type="text" name="txtRg" id="txtRg" placeholder="Rg" /><br><br>
-        <label>Ativo :</label><SELECT  name="txtAtivo" id= "txtAtivo"><OPTION>SIM <OPTION>NÃO </SELECT> <br><br>
+        <input type="password" class="form-control" name="txtSenha" id="txtSenha" placeholder="Senha" /><br><br>
+        <input type="date" class="form-control" name="txtNascimento" id="txtNascimento" data-mask="00/00/0000" maxlength="8" placeholder="Nacimento DD/MM/AAAA" /><br><br>
+        <input type="text" class="form-control" name="txtRg" id="txtRg" placeholder="Rg" /><br><br>
+        <label>Ativo :</label><SELECT  name="txtAtivo" class="form-control" id= "txtAtivo"><OPTION>SIM <OPTION>NÃO </SELECT> <br><br>
 
-        <input type="submit" value="Cadastrar"/>
-        <input type="reset" value="Limpar"/>
+        
+        <input type="submit" class="btn btn-success" value="Cadastrar"/>
+        <input type="reset" class="btn btn-warning" value="Limpar"/>
     </form>
+    </div>
     </div>
     </body>
 
