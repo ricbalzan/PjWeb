9f2bc506fc65bb50594b63de74f232d4a5f189a1
ricbalzan <balzanricardo@gmail.com>
2021-05-13 21:59:05 -0300

    listar usuario

---
+++ View/User/listarUsuario.php
@@ -0,0 +1,40 @@
+<section>
+<link rel="stylesheet" type="text/css" href="../templates/bootstrap/css/bootstrap.css" />
+    <h3>
+        <span>Listar Usuários</span>
+    </h3>
+</section>
+<section class="content">
+    <div class="box">
+        <div class="box-header whit-border">
+            <div class="row margin-botton-28">
+                <div class="col-md-12 text-right">
+                    <input href="cadUsuario.php" type="submit" class="btn btn-success" value="Novo"/>
+                </div>
+            </div>
+        </div>
+    </div>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>CPF</th>
+                <th>NOME</th>
+                <th>EMAIL</th>
+                <th>NASCIMENTO</th>
+                <th>RG</th>
+                <th>ATIVO</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>CPF</td>
+                <td>NOME</td>
+                <td>EMAIL</td>
+                <td>NASCIMENTO</td>
+                <td>RG</td>
+                <td>ATIVO</td>
+            </tr>
+        </tbody>
+    </table>
+
+</section>
