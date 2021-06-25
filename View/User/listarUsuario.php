d496ab72aa26b61683a433578a9f844a3c0083bd
ricbalzan <balzanricardo@gmail.com>
2021-06-25 08:27:43 -0300

    Update listarUsuario.php

--- View/User/listarUsuario.php
+++ View/User/listarUsuario.php
@@ -7,10 +7,9 @@
         <meta charset="UTF-8">
         <title>Consulta de Usuários</title>
     </head>
-    <table class="table table-condensed">
-    <body>
-   
 
+
+    <body>
         <?php
         if (isset($_SESSION['users'])) {
             include_once '../../Model/Usuario.php';
@@ -24,10 +23,10 @@
                 echo "<tr><td><a href='../../Controller/UserController.php?operation=deletar&id=$cpf'>Deletar</a></td> - $nomeCompleto<br></tr> ";
             }
             unset($_SESSION['user']);
-
+            
         }
-        </table>
+        
         ?>
     </body>
-    </table>
+
 </html>
